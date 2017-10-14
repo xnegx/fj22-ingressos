e1285bcfe3030c91df63d3af0a10f99e8a554d83
Everton <negramones@gmail.com>
2017-10-14 09:57:19 -0300

    exercicionos terminados - funcionando

--- src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
+++ src/main/java/br/com/caelum/ingresso/model/form/SessaoForm.java
@@ -6,6 +6,12 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+
 public class SessaoForm {
 	
 	private Integer id;
@@ -50,6 +56,18 @@ public class SessaoForm {
 
 	public void setFilmeId(Integer filmeId) {
 		this.filmeId = filmeId;
+	}
+	
+	public Sessao toSessao(SalaDao salaDao, FilmeDao filmeDao) {
+		Filme filme = filmeDao.findOne(filmeId);
+		Sala sala = salaDao.findOne(salaId);
+		
+		Sessao sessao = new Sessao(horario, filme, sala);
+		sessao.setId(id);
+		
+		return sessao;
+		
+		
 	}
 
 }
