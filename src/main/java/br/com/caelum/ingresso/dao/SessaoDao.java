a9e7eb0daf53a5f735d9635e85639c3ad7cc66f5
Everton <negramones@gmail.com>
2017-10-21 11:11:20 -0200

    exercicio 4.3

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -8,6 +8,7 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
@@ -25,6 +26,11 @@ public class SessaoDao {
 				.setParameter("sala", sala)
 				.getResultList();
 		
+	}
+	
+	public List<Sessao> buscaSessoesDoFIlme(Filme filme) {
+		return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
+				.setParameter("filme",filme).getResultList();
 	}
 	
 
---
+++ src/main/java/br/com/caelum/ingresso/model/DetalhesDoFilme.java
@@ -0,0 +1,5 @@
+package br.com.caelum.ingresso.model;
+
+public class DetalhesDoFilme {
+
+}
